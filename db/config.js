620908ec741be7ed40519180fffa6b034984fa7a
Olatunji Yusuf <olatunjiyso@gmail.com>
2021-11-07 16:02:26 +0100

    setup database connection

---
+++ db/config.js
@@ -0,0 +1,12 @@
+const dotenv = require('dotenv');
+
+dotenv.config();
+const dbUrls = {
+    development: process.env.DEVELOPMENT_DB,
+    test: process.env.TEST_DB,
+    production: process.env.PRODUCTION_DB
+}
+
+const environment = process.env.NODE_ENV;
+const dbUrl = dbUrls[environment];
+module.exports = {environment, dbUrl}
---
+++ db/index.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+const { dbUrl, environment } = require('./config');
+
+const connect = ()=> {
+    mongoose.connect(dbUrl, null, (err)=> {
+        if(err) { 
+            console.log(`An error occured while connecting to db on ${environment} environment`, err)
+        }
+    });
+}
+const disconnect = ()=> { mongoose.connection.close() }
+
+module.exports = { connect, disconnect }
