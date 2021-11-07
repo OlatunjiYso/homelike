bacbe29663c110517dd1f7dbdfcc61c43c194d72
Olatunji Yusuf <olatunjiyso@gmail.com>
2021-11-07 15:52:28 +0100

    update apartment model

---
+++ Model/apartment.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// const GeoSchema = new Schema({
+//     type: { type: 'String', default: 'Point' },
+//     coordinates: { type: [Number], index: '2dsphere' }
+// });
+
+const pointSchema = Schema({
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true
+    },
+    coordinates: {
+      type: [Number],
+      required: true
+    }
+  });
+
+const ApartmentSchema = new Schema({
+    description: { type: String },
+    rooms: { type: Number, default: 1 },
+    country: { type: String },
+    city: { type: String },
+    geometry: {
+        type: pointSchema,
+        required: true,
+        index: '2dsphere'
+    },
+    addedBy: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    }
+});
+
+
+const Apartment = mongoose.model('apartment', ApartmentSchema);
+module.exports = { Apartment, ApartmentSchema };
